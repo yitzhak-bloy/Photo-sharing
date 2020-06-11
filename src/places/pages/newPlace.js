d299e0934183c7694d39b9dc2a6227474a678c07
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-06-11 14:04:17 +0300

    Define page routing with react-router-dom

---
+++ src/places/pages/newPlace.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewPlace = () => {
+  return <h2>מקום חדש</h2>
+}
+
+export default NewPlace;
---
+++ src/user/pages/users.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Users = () => {
+    return <h1>!הניתוב הצליח</h1>
+}
+
+export default Users
