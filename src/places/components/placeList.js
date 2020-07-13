ea4f561b63b6f6de468337eb9e91a8d659f622b5
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-07-13 15:51:49 +0300

    adding PlaceList and PlaceItem component

---
+++ src/places/components/placeList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Card from '../../shared/components/UIElements/Card';
+import PlaceItem from './PlaceItem';
+import './PlaceList.css';
+
+const PlaceList = props => {
+  if (props.items.length === 0) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h1>no places found, enter some places</h1>
+          <button>enter place</button>
+        </Card>
+      </div>
+    );
+  }
+
+  return ( 
+    <ul className="place-list">
+      {props.items.map(place => (
+        <PlaceItem 
+          kay={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          title={place.title}
+          description={place.description}
+          eddress={place.address}
+          creatorId={place.creator}
+          coordinates={place.location}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default PlaceList;
