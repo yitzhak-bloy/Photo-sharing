146705cf752b98fd89ac3abdcd179b8b95c58d2c
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-08-07 13:01:08 +0300

    Adjusting the Form Hook

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
@@ -40,24 +40,43 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
+  const [isLoading, setIsLoading] = useState(true)
   const placeId = useParams().placeId;
 
-  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
-
-  const [formState, inputHandler] = useForm(
+  const [formState, inputHandler, setFormData] = useForm(
     {
       title: {
-        value: identifiedPlace.title,
-        isValid: true
+        value: '',
+        isValid: false
       },
       description: {
-        value: identifiedPlace.description,
-        isValid: true
+        value: '',
+        isValid: false
       }
     },
-    true
+    false
   );
+    
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
 
+  useEffect(() => {
+    setFormData(
+      {
+        title: {
+          value: identifiedPlace.title,
+          isValid: true
+        },
+        description: {
+          value: identifiedPlace.description,
+          isValid: true
+        }
+      },
+      true
+    );
+    setIsLoading(false);
+  }, [setFormData, identifiedPlace])
+
+  
   const placeUpdateSubmitHandler = event => {
     event.preventDefault();
     console.log(formState.inputs);
@@ -69,6 +88,14 @@ const UpdatePlace = () => {
         <h2>Could not find place!</h2>
       </div>
     );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading...</h2>
+      </div>
+    );    
   }
 
   return (
