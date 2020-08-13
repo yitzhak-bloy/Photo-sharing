67de7fccf4d7e5e676a581e3679c6a7a4a62958d
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-08-13 15:11:58 +0300

    Adding an "Auth" Page 2

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,7 +1,46 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import {
+  VALIDATOR_EMAIL
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+
 const Auth = () => {
-  return <h1>!אימות המשתמש</h1>
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      },
+      address: {
+        value: '',
+        isValid: false
+      }
+    },
+    false
+  );
+
+  return (
+    <form className="place-form">
+      <Input
+        id="mail"
+        element="input"
+        type="text"
+        label="Mail"
+        validators={[VALIDATOR_EMAIL()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
+    </form>)
 };
 
 export default Auth;
