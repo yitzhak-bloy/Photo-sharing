9edc7c68168ee7003e9e136821954c59376815a0
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-08-23 14:00:58 +0300

    Adding Place-Specific Routes

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -2,9 +2,39 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Place rout work');
-  res.json({message: 'It works!'})
+const DUMMY_PLACES = [
+    {
+      id: 'p1',
+      title: 'Empire State Building',
+      description: 'One of the most famous sky scrapers in the world!',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+      address: '20 W 34th St, New York, NY 10001',
+      location: {
+        lat: 40.7484405,
+        lng: -73.9878584
+      },
+      creator: 'u1'
+    },
+    {
+      id: 'p2',
+      title: 'Empire State Building',
+      description: 'One of the most famous sky scrapers in the world!',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+      address: '20 W 34th St, New York, NY 10001',
+      location: {
+        lat: 40.7484405,
+        lng: -73.9878584
+      },
+      creator: 'u2'
+    }
+  ];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  res.json({place})
 });
 
 module.exports = router;
