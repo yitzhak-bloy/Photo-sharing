9bc7c62b60ac21b322184e232187539dd151dbc8
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-08-23 22:12:37 +0300

    Getting a Place By User ID

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -25,15 +25,19 @@ const DUMMY_PLACES = [
         lat: 40.7484405,
         lng: -73.9878584
       },
-      creator: 'u2'
+      creator: 'u1'
     }
   ];
 
 router.get('/:pid', (req, res, next) => {
   const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find(p => {
-    return p.id === placeId;
-  });
+  const place = DUMMY_PLACES.find(p => p.id === placeId);
+  res.json({place})
+});
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find(p => p.creator === userId);
   res.json({place})
 });
 
