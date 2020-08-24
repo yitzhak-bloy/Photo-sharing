6b96169c5a2890b255b82bd7a99c3ae18e1e7acf
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-08-24 15:23:37 +0300

    Handling Errors 2

--- server/app.js
+++ server/app.js
@@ -5,14 +5,14 @@ const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use('/api/places', placesRoutes);
+app.use('/api/places', placesRoutes); // => /api/places...
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
   res.status(error.code || 500)
-  res.json({massage: error.massage || 'An unknuwn error occurred'})
+  res.json({message: error.message || 'An unknown error occurred!'});
 });
 
 app.listen(5000);
--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -3,53 +3,49 @@ const express = require('express');
 const router = express.Router();
 
 const DUMMY_PLACES = [
-    {
-      id: 'p1',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-      address: '20 W 34th St, New York, NY 10001',
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584
-      },
-      creator: 'u1'
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
     },
-    {
-      id: 'p2',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-      address: '20 W 34th St, New York, NY 10001',
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584
-      },
-      creator: 'u1'
-    }
-  ];
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
+];
 
 router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find(p => p.id === placeId);
+  const placeId = req.params.pid; // { pid: 'p1' }
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+
   if (!place) {
-    const error = new Error('Could not find a place for the provider id.')
+    const error = new Error('Could not find a place for the provided id.');
     error.code = 404;
-    throw(error);
+    throw error;
   }
-  res.json({place})
+
+  res.json({ place }); // => { place } => { place: place }
 });
 
 router.get('/user/:uid', (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find(p => p.creator === userId);
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+
   if (!place) {
-    const error = new Error('Could not find a place for the provider user id.')
+    const error = new Error('Could not find a place for the provided user id.');
     error.code = 404;
     return next(error);
   }
 
-  res.json({place})
+  res.json({ place });
 });
 
 module.exports = router;
