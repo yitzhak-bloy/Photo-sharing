7f3770da55cd30a5c5db25ad87cdf33c488206ff
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-08-24 15:20:50 +0300

    Handling Errors

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -32,12 +32,23 @@ const DUMMY_PLACES = [
 router.get('/:pid', (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find(p => p.id === placeId);
+  if (!place) {
+    const error = new Error('Could not find a place for the provider id.')
+    error.code = 404;
+    throw(error);
+  }
   res.json({place})
 });
 
 router.get('/user/:uid', (req, res, next) => {
   const userId = req.params.uid;
   const place = DUMMY_PLACES.find(p => p.creator === userId);
+  if (!place) {
+    const error = new Error('Could not find a place for the provider user id.')
+    error.code = 404;
+    return next(error);
+  }
+
   res.json({place})
 });
 
