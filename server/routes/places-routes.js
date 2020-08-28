e165019adaeebd3baab1cd25ed1cadef612e6763
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-08-28 11:34:01 +0300

    Deleting Places

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -12,5 +12,7 @@ router.post('/', placesControllers.createPlace);
 
 router.patch('/:pid', placesControllers.updatePlace)
 
+router.delete('/:pid',placesControllers.deletePlace)
+
 
 module.exports = router;
