92bc47c0e6569a7b3231fe17b75edd86abbf2f81
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-09-06 20:40:00 +0300

    fix

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -21,7 +21,9 @@ let DUMMY_PLACES = [
 const getPlaceById = (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
 
-  const place = DUMMY_PLACES.find(p => p.id === placeId);
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
 
   if (!place) {
     throw new HttpError('Could not find a place for the provided id.', 404);
@@ -30,37 +32,43 @@ const getPlaceById = (req, res, next) => {
   res.json({ place }); // => { place } => { place: place }
 };
 
+// function getPlaceById() { ... }
+// const getPlaceById = function() { ... }
+
 const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const places = DUMMY_PLACES.filter(p => p.creator === userId);
+  const places = DUMMY_PLACES.filter(p => {
+    return p.creator === userId;
+  });
 
   if (!places || places.length === 0) {
-    return next(new HttpError('Could not find a place for the provided user id.', 404))
+    return next(
+      new HttpError('Could not find places for the provided user id.', 404)
+    );
   }
 
-  res.json({ place: places });
-}
+  res.json({ places });
+};
 
 const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
-
-  if (!errors.isEmpty()) { 
+  if (!errors.isEmpty()) {
     next(new HttpError('Invalid inputs passed, please check your data.', 422));
-  };
+  }
 
-  const {title, description, address, creator} = req.body;
-  
+  const { title, description, address, creator } = req.body;
+
   let coordinates;
-
   try {
     coordinates = await getCoordsForAddress(address);
   } catch (error) {
     return next(error);
   }
 
-  const createPlace = {
-    id: uuidv4(),
+  // const title = req.body.title;
+  const createdPlace = {
+    id: uuid(),
     title,
     description,
     location: coordinates,
@@ -68,42 +76,41 @@ const createPlace = async (req, res, next) => {
     creator
   };
 
-  DUMMY_PLACES.push(createPlace);
+  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
 
-  res.status(201).json({place: createPlace})
+  res.status(201).json({ place: createdPlace });
 };
 
 const updatePlace = (req, res, next) => {
   const errors = validationResult(req);
-
-  if (!errors.isEmpty()) { 
+  if (!errors.isEmpty()) {
     throw new HttpError('Invalid inputs passed, please check your data.', 422);
-  };
+  }
 
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatePlace = {...DUMMY_PLACES.find(p => p.id === placeId)};
-  placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
-  updatePlace.title = title;
-  updatePlace.description = description;
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
 
-  DUMMY_PLACES[placeIndex] = updatePlace;
+  DUMMY_PLACES[placeIndex] = updatedPlace;
 
-  res.status(200).json({place: updatePlace});
+  res.status(200).json({ place: updatedPlace });
 };
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
   if (!DUMMY_PLACES.find(p => p.id === placeId)) {
-    throw new HttpError('Could not find a place for that id.', 404)
+    throw new HttpError('Could not find a place for that id.', 404);
   }
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
-  res.status(200).json({ massege: 'Deleted place.' });
-}
+  res.status(200).json({ message: 'Deleted place.' });
+};
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlacesByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
--- server/util/location.js
+++ server/util/location.js
@@ -4,23 +4,30 @@ const HttpError = require('../models/http-error');
 
 const API_KEY = "AIzaSyAFMsHjpEycZlxQgoTFLqmixZMAhHjhwzc";
 
-async const getCoordsForAddress = (address) => {	
+async function getCoordsForAddress(address) {
+  // return {
+  //   lat: 40.7484474,
+  //   lng: -73.9871516
+  // };
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
 
-	const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`)
+  const data = response.data;
 
-	const date = response.date;
+  if (!data || data.status === 'ZERO_RESULTS') {
+    const error = new HttpError(
+      'Could not find location for the specified address.',
+      422
+    );
+    throw error;
+  }
 
-	if (!date || date.status === 'ZERO_RESULTS') {
-		const error = new HttpError(
-			'Cold not find location for the specified address.', 
-			422
-		);
-		throw error;
-	}
+  const coordinates = data.results[0].geometry.location;
 
-	const coordinates = data.results[0].geometry.location;
-
-	return coordinates;
-};
+  return coordinates;
+}
 
 module.exports = getCoordsForAddress;
