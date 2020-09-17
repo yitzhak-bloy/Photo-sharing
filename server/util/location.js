a9c024654e3433a113cbf93225ee7fc46c5bfa9a
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-09-17 12:32:07 +0300

    Creating the Place Schema & Model

--- server/util/location.js
+++ server/util/location.js
@@ -5,22 +5,30 @@ const HttpError = require('../models/http-error');
 const API_KEY = "AIzaSyCxrRe6TKSHCqzXLfrXqtyaZHV3o0sHncg";
 
 async function getCoordsForAddress(address) {
-  const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURIComponent(
-    address
-  )}&inputtype=textquery&fields=geometry&key=${API_KEY}`;
- 
-  const response = await axios.get(url);
- 
+  // return {
+  //   lat: 40.7484474,
+  //   lng: -73.9871516
+  // };
+  // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCxrRe6TKSHCqzXLfrXqtyaZHV3o0sHncg
+  
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+
   const data = response.data;
-  if (!data || data.status === "ZERO_RESULTS") {
+
+  if (!data || data.status === 'ZERO_RESULTS') {
     const error = new HttpError(
-      "Could not find location for the specified address.",
+      'Could not find location for the specified address.',
       422
     );
     throw error;
   }
+
   const coordinates = data.results[0].geometry.location;
- 
+
   return coordinates;
 }
 
