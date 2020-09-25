32bc8c6165dd467375dbb3d9856071f22c2942de
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-09-25 08:59:48 +0300

    Getting Places - An Alternative

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -51,9 +51,9 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate('places')
   } catch (err) {
     const error = new HttpError(
       'Fetching places failed, please try again later.', 
@@ -62,13 +62,13 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError('Could not find places for the provided user id.', 404)
     );
   }
 
-  res.json({ places: places.map(place => place.toObject({ getters: true })) });
+  res.json({ places: userWithPlaces.places.map(place => place.toObject({ getters: true })) });
 };
 
 
