b33981fabe9bc5e0f49c5e1ad4a1c3aefb52fb75
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-09-29 22:37:00 +0300

    Handling CORS Errors

--- server/app.js
+++ server/app.js
@@ -10,28 +10,40 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route', 404);
+  const error = new HttpError('Could not find this route.', 404);
   throw error;
-})
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-
 mongoose
-  .connect('mongodb+srv://manu:nkfh1993@cluster0.dvrwq.gcp.mongodb.net/places?retryWrites=true&w=majority')
+  .connect(
+    `mongodb+srv://manu:nkfh1993@cluster0.dvrwq.gcp.mongodb.net/marn?retryWrites=true&w=majority`
+  )
   .then(() => {
     app.listen(5000);
   })
   .catch(err => {
     console.log(err);
-  })
+  });
