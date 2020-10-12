ba1e1438f4233c8675a4196f32d338c89d83a23a
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-10-12 18:17:57 +0300

    Final Adjustments

--- client/src/places/components/PlaceItem.js
+++ client/src/places/components/PlaceItem.js
@@ -61,8 +61,8 @@ const PlaceItem = props => {
         footerClass="place-item__modal-actions" 
         footer={
           <React.Fragment>
-            <Button inverse onClick={cancelDeleteHandler}>ביטול</Button>
-            <Button danger onClick={confirmDeleteHandler} >מחיקה</Button>
+            <Button inverse onClick={cancelDeleteHandler}>EDIT</Button>
+            <Button danger onClick={confirmDeleteHandler} >DELETE</Button>
           </React.Fragment>
         } 
       >
@@ -83,8 +83,8 @@ const PlaceItem = props => {
           </div>
           <div className="place-item__actions">
             <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-            {auth.isLoggedIn && <Button to={`/places/${props.id}`}>EDIT</Button>}
-            {auth.isLoggedIn && <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>}
+            {auth.userId === props.creatorId && <Button to={`/places/${props.id}`}>EDIT</Button>}
+            {auth.userId === props.creatorId && <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>}
           </div>
         </Card>
       </li>
