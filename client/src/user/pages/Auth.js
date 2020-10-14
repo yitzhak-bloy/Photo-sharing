f26a2ea9bc27a9c19889c0ee482c2f872c4b8b92
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-10-14 09:26:19 +0300

    Finishing & Using the ImageUpload Component (in a Form)

--- client/src/user/pages/Auth.js
+++ client/src/user/pages/Auth.js
@@ -41,7 +41,8 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined
+          name: undefined,
+          image: undefined
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -51,6 +52,10 @@ const Auth = () => {
           ...formState.inputs,
           name: {
             value: '',
+            isValid: false
+          },
+          image: {
+            value: null,
             isValid: false
           }
         },
@@ -63,6 +68,8 @@ const Auth = () => {
   const authSubmitHandler = async event => {
     event.preventDefault();
     
+    console.log(formState.inputs);
+
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
@@ -116,7 +123,7 @@ const Auth = () => {
               onInput={inputHandler}
             />
           )}
-          {!isLoginMode && <ImageUpload center id='image' />}
+          {!isLoginMode && <ImageUpload center id='image' onInput={inputHandler} />}
           <Input
             element="input"
             id="email"
